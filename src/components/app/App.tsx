6b663742d8317003865d9bda527e0afafe8205ab
Alex Gorodov <a.gorodov.22@gmail.com>
2023-06-07 08:52:32 +0300

    deploy to prod

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,7 @@
 import { HomePage } from "../../pages/home-page/home-page";
 import { FC } from "react";
 import { AppRoute } from "../../const";
-import { BrowserRouter, HashRouter, Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import browserHistory from "../../browser-history";
 import { HistoryRouter } from "../history-route/history-route";
 import { Provider } from "react-redux";
