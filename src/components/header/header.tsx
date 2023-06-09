636fcfc227aa591a4b7719ac9afe9ba91bd61347
Alex Gorodov <a.gorodov.22@gmail.com>
2023-06-09 22:40:15 +0300

    header and poopup repositioned

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import './header.sass';
 import { useSelector } from 'react-redux';
+import cn from 'classnames';
 import { RootState } from '../../store/RootState';
 import { DISABLED_DAYS } from '../../mocks/calendar';
 import { MAX_UNAVAILABLE, StatusesValues } from '../../const';
@@ -16,6 +17,10 @@ export function Header(): JSX.Element {
   const dateTo = calendar[calendar.length - DISABLED_DAYS - 1].date;
   
   const [offset, setOffset] = useState(0);
+
+  const headerContentClassName = cn('header__content-wrapper', {
+    'header__content-wrapper--collapsed' : offset > 100,
+  })
 
   useEffect(() => {
     const onScroll = () => setOffset(window.pageYOffset);
@@ -36,58 +41,39 @@ export function Header(): JSX.Element {
         </div>
         <p className="header__subtitle">שלום, {userInfo ? userInfo.firstName : ''}</p>
       </div>
-      {
-        offset < 100
-          ? <div className="header__content-wrapper">
-              <div className="header__description-wrapper">
-                <p className="header__description">
-                  <b>
-                    הודעה חשובה
-                  </b>
-                  ! הצו עבור האימון והתעסוקה חל בין התאריכים:
-                  <br />
-                  {dateFrom.getMonth() === dateTo.getMonth()
-                    ? `${formatDate(dateTo, 'DD/MM/YYYY')} - ${dateFrom.getDate() < 10 ? '0' + dateFrom.getDate() : dateFrom.getDate()}`
-                    : `${formatDate(dateTo, 'DD/MM/YYYY')} - ${formatDate(dateFrom, 'DD/MM')}`}</p><p className="header__description">
-                    בשעון הלחימה תוכל לחסום ימי מילואים בהם אינך יכול להתייצב.<br />
-                    בתעסוקה זו באפשרותך לחסום עד {MAX_UNAVAILABLE} ימי מילואים.
-                    <br /><b>
-                      שים לב! קיים מספר מוגבל של ימים אותם ניתן לחסום.<br />
-                      קיימים ימים נעולים אליהם אתה מחויב להתייצב.
-                    </b>
-                  </p>
-                </div>
-              
-              <p className="header__description--summary">
-                הנך מתייצב ל:&nbsp;
-                {
-                  calendar.filter
-                  ((card) => 
-                    card.status === StatusesValues.Available
-                    || card.status === StatusesValues.Locked
-                    || card.status === StatusesValues.Partly
-                  ).length
-                }
-              &nbsp;ימי מילואים
-              </p>
-            </div>
-          : <div className="header__content-wrapper">
-              {/* <p className="header__subtitle">שלום, {userInfo ? userInfo.firstName : ''}</p> */}
-              <p className="header__description--summary">
-                הנך מתייצב ל:&nbsp;
-                {
-                  calendar.filter
-                  ((card) => 
-                    card.status === StatusesValues.Available
-                    || card.status === StatusesValues.Locked
-                    || card.status === StatusesValues.Partly
-                  ).length
-                }
-              &nbsp;ימי מילואים
-              </p>
-            </div>
-      }
-      
+      <div className={headerContentClassName}>
+        <div className="header__description-wrapper">
+          <p className="header__description">
+            <b>
+              הודעה חשובה
+            </b>
+            ! הצו עבור האימון והתעסוקה חל בין התאריכים:
+            <br />
+            {dateFrom.getMonth() === dateTo.getMonth()
+              ? `${formatDate(dateTo, 'DD/MM/YYYY')} - ${dateFrom.getDate() < 10 ? '0' + dateFrom.getDate() : dateFrom.getDate()}`
+              : `${formatDate(dateTo, 'DD/MM/YYYY')} - ${formatDate(dateFrom, 'DD/MM')}`}</p><p className="header__description">
+              בשעון הלחימה תוכל לחסום ימי מילואים בהם אינך יכול להתייצב.<br />
+              בתעסוקה זו באפשרותך לחסום עד {MAX_UNAVAILABLE} ימי מילואים.
+              <br /><b>
+                שים לב! קיים מספר מוגבל של ימים אותם ניתן לחסום.<br />
+                קיימים ימים נעולים אליהם אתה מחויב להתייצב.
+              </b>
+            </p>
+          </div>
+        
+        <p className="header__description--summary">
+          הנך מתייצב ל:&nbsp;
+          {
+            calendar.filter
+            ((card) => 
+              card.status === StatusesValues.Available
+              || card.status === StatusesValues.Locked
+              || card.status === StatusesValues.Partly
+            ).length
+          }
+        &nbsp;ימי מילואים
+        </p>
+      </div>
     </header>
   );
 }
